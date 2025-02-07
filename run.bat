7d4bd1800acb681662d0a981d218d88322e33d69
noelav07 <noel.a.v.07@gmail.com>
2025-02-07 21:43:33 +0530

    run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-@echooff
-docker run -d -p 8501:8501 -p  8502:8502 -p 8503:8503 --name cra noelav07/career_roadmap_assistance 
+@echo off
+docker run -d -p 8501:8501 -p 8502:8502 -p 8503:8503 --name cra noelav07/career_roadmap_assistance
