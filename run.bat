05bc2a9301d666f98257b4c51ef31ab9a9ef3aed
noelav07 <noel.a.v.07@gmail.com>
2025-02-28 15:47:19 +0530

    follow up local files

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 start "App 1" cmd /k streamlit run app.py --server.port 8501
 start "App 2" cmd /k streamlit run map.py --server.port 8502
-start "App 3" cmd /k streamlit run bot.py --server.port 8503
+start "App 3" cmd /k python flask_server.py port 8503
